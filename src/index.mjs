e232f0814de6ea1943ccd2d9e320fcd5376e9075
Andre Bölts <volto@localhost.localdomain>
2018-02-02 20:07:24 +0100

    Generate NOW Valid for multipart/form-data requests

--- src/index.mjs
+++ src/index.mjs
@@ -77,93 +77,7 @@ const defaultHttpOptions = {
   includeExtensions: false
 }
 
-export const jsonUploadLink = ({
-  includeExtensions,
-  uri: linkUri = '/graphql',
-  credentials: linkCredentials,
-  headers: linkHeaders,
-  fetchOptions: linkFetchOptions = {},
-  fetch: linkFetch = fetch
-} = {}) =>
-  new ApolloLink(
-    ({ operationName, variables, query, extensions, getContext, setContext }) =>
-      new Observable(observer => {
-        const requestOperation = { query: print(query) }
-
-        if (operationName) requestOperation.operationName = operationName
-        if (Object.keys(variables).length)
-          requestOperation.variables = variables
-        if (extensions && includeExtensions)
-          requestOperation.extensions = extensions
-
-        const files = extractFiles(requestOperation)
-
-        const {
-          uri = linkUri,
-          credentials = linkCredentials,
-          headers: contextHeaders,
-          fetchOptions: contextFetchOptions = {}
-        } = getContext()
-
-        const fetchOptions = {
-          ...linkFetchOptions,
-          ...contextFetchOptions,
-          headers: {
-            ...linkFetchOptions.headers,
-            ...contextFetchOptions.headers,
-            ...linkHeaders,
-            ...contextHeaders
-          },
-          method: 'POST'
-        }
-
-        if (credentials) fetchOptions.credentials = credentials
-
-        if (files.length) {
-          // GraphQL multipart request spec:
-          // https://github.com/jaydenseric/graphql-multipart-request-spec
-
-          fetchOptions.body = new FormData()
-
-          fetchOptions.body.append(
-            'operations',
-            JSON.stringify(requestOperation)
-          )
-
-          fetchOptions.body.append(
-            'map',
-            JSON.stringify(
-              files.reduce((map, { path }, index) => {
-                map[`${index}`] = [path]
-                return map
-              }, {})
-            )
-          )
-
-          files.forEach(({ file }, index) =>
-            fetchOptions.body.append(index, file, file.name)
-          )
-        } else {
-          fetchOptions.headers['content-type'] = 'application/json'
-          fetchOptions.body = JSON.stringify(requestOperation)
-        }
-
-        linkFetch(uri, fetchOptions)
-          .then(response => {
-            setContext({ response })
-            if (!response.ok)
-              throw new Error(`${response.status} (${response.statusText})`)
-            return response.json()
-          })
-          .then(result => {
-            observer.next(result)
-            observer.complete()
-          })
-          .catch(error => observer.error(error))
-      })
-  )
-
-export const multipartUploadLink = (linkOptions = {}) => {
+export const createUploadLink = (linkOptions = {}) => {
   let {
     uri,
     fetch: fetcher,
@@ -189,22 +103,14 @@ export const multipartUploadLink = (linkOptions = {}) => {
         } = operation.getContext()
         const { operationName, extensions, variables, query } = operation
         const http = { ...defaultHttpOptions, ...httpOptions }
-        // const body = { operationName, variables };
 
         const printedQuery = print(query)
         const body = new FormData()
-        const files = extractFiles(variables)
-        body.append(
-          'files',
-          JSON.stringify(
-            files.reduce((map, { path }, index) => {
-              map[`${index}`] = [path]
-              return map
-            }, {})
-          )
-        )
 
-        files.forEach(({ file }, index) => body.append(index, file, file.name))
+        const files = extractFiles(variables)
+        if (files.length)
+          files.forEach(({ file }) => body.append('file', file, file.name))
+
         body.append('operationName', operationName)
         try {
           body.append('variables', JSON.stringify(variables))
@@ -230,8 +136,7 @@ export const multipartUploadLink = (linkOptions = {}) => {
           ...options,
           headers: {
             // headers are case insensitive (https://stackoverflow.com/a/5259004)
-            accept: '*/*',
-            'content-type': 'multipart/form-data'
+            accept: '*/*'
           },
           body: body
         }
@@ -277,10 +182,4 @@ export const multipartUploadLink = (linkOptions = {}) => {
         }
       })
   )
-}
-
-export const createUploadLink = ({ multipart, ...props }) => {
-  if (multipart) return multipartUploadLink(props)
-
-  return jsonUploadLink(props)
 }
